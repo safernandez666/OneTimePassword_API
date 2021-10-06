6dfc4fd9fb245473a2905871d2856cb485512e88
Fernandez Santiago <santiago@MacBook-Pro-de-Fernandez.local>
2021-10-06 11:24:10 -0300

    Version 1.1

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import user_controller
 import os, math, random, smtplib
 from datetime import datetime
 # Import the email modules we'll need
 from email.message import EmailMessage
+from functools import wraps
 
 app = Flask(__name__) 
 
@@ -13,7 +14,42 @@ smtp_port = 587
 email_sender = os.getenv('EMAIL_USER')
 email_password = os.getenv('EMAIL_PASSWORD')
 timeToLease = 5 # Time to change the OTP. It's in Minutes.
+app.config['API_KEY'] = os.getenv('API_KEY', None)
 
+######################################################################
+# HTTP Error Handlers
+######################################################################
+@app.errorhandler(401)
+def not_authorized(e):
+    """ Respose sent back when not autorized """
+    return jsonify(status=401, error='Not Authorized',
+                   message='You are authorized to access the URL requested.'), 401
+
+######################################################################
+# Check Auth: Add your autorization code here
+######################################################################
+def check_auth():
+    """ Checks the environment that the API_KEY has been set """
+    if app.config['API_KEY']:
+        return app.config['API_KEY'] == request.headers.get('X-Api-Key')
+    return False
+
+######################################################################
+# Requires API Key: Decorator function to add secuity to any call
+######################################################################
+def requires_apikey(f):
+    """ Decorator function to require API Key """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        """ Decorator function that does the checking """
+        if check_auth():
+            return f(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated
+######################################################################
+# Functions
+######################################################################   
 # Generate One Time Password
 def generateOneTimePassword():
     digits="0123456789"
@@ -49,9 +85,12 @@ def delta(time, timeToLease):
         return True
     else:
         return False
-
+######################################################################
+# Routes & Services
+######################################################################  
 # Iniciate Process
 @app.route("/v1/init", methods=["GET"])
+@requires_apikey
 def update_otp():
     user_details = request.get_json()
     email = user_details["email"]
@@ -65,7 +104,8 @@ def update_otp():
 
 # Validete email & Code
 @app.route("/v1/validate", methods=["POST"])
-def validate():
+@requires_apikey
+def validate(): 
     user_details = request.get_json()
     email = user_details["email"]
     code = user_details["code"]
@@ -77,6 +117,8 @@ def validate():
         else:
             return jsonify({'message': 'Bad Request'}), 404        
     return jsonify({'message': 'This email dont exist'}), 404
-
+######################################################################
+# Main Program
+######################################################################  
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
